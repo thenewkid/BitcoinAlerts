6631aacb83ca0f51317dc251dc8c2c71d1ba3a7c
gurujilove108 <cyberkid108@gmail.com>
2017-12-20 02:19:24 -0800

    added files

--- src/main/java/dannen/crypto/notify/CryptoCurrencyNotifyApplication.java
+++ src/main/java/dannen/crypto/notify/CryptoCurrencyNotifyApplication.java
@@ -1,4 +1,4 @@
-package com.bitcoin.alerts.CryptoCurrencyNotify;
+package dannen.crypto.notify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
