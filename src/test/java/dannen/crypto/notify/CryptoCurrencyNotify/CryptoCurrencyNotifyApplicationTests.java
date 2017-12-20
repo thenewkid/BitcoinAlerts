138f54153547d15893544279f14b8dce6cfa75fe
gurujilove108 <cyberkid108@gmail.com>
2017-12-20 06:17:37 -0800

    Up and running

--- src/test/java/dannen/crypto/notify/CryptoCurrencyNotify/CryptoCurrencyNotifyApplicationTests.java
+++ src/test/java/dannen/crypto/notify/CryptoCurrencyNotify/CryptoCurrencyNotifyApplicationTests.java
@@ -1,6 +1,6 @@
-package com.bitcoin.alerts.CryptoCurrencyNotify;
-
+package dannen.crypto.notify.CryptoCurrencyNotify;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
