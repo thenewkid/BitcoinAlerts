631d6c711559b5ab6f65cef5d3021e99d7e7bdd5
gurujilove108 <cyberkid108@gmail.com>
2017-12-20 01:15:20 -0800

    Added basic spring boot 1.5.9 app

---
+++ src/main/java/com/bitcoin/alerts/CryptoCurrencyNotify/CryptoCurrencyNotifyApplication.java
@@ -0,0 +1,12 @@
+package com.bitcoin.alerts.CryptoCurrencyNotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCurrencyNotifyApplication.class, args);
+	}
+}
